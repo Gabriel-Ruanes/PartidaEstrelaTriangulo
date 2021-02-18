53660990551479327782f7bfe8cad92c575d684e
21192694 <21192694@DSN1271158611.cfp127aluno.local>
2021-02-18 15:38:50 -0200

    Inicialização para a programação partida estrela triangulo.

---
+++ PartidaEstrelaTriangulo.X/main.c
@@ -0,0 +1,18 @@
+/*
+ * File:   main.c
+ * Author: 21192694
+ *
+ * Created on 18 de Fevereiro de 2021, 15:36
+ */
+
+
+#include <xc.h>
+#include "config.h"
+
+void main(void) 
+{
+    while ( 1 )
+    {
+    
+    }
+}
